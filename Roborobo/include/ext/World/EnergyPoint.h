3b960b2c8708979c299b4b73ac1000c218ea44f9
Andreas Hagen <andhag@rocks.hpc.ntnu.no>
2014-05-09 11:14:41 +0200

    Added Roborobo

---
+++ Roborobo/include/ext/World/EnergyPoint.h
@@ -0,0 +1,82 @@
+/*
+ *  EnergyPoint.h
+ *  roborobo
+ *
+ *  Created by Jean-Marc on 11/03/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef ENERGYPOINT_H
+#define ENERGYPOINT_H
+
+#include "RoboroboMain/common.h"
+#include "Utilities/Misc.h"
+#include "World/InanimateObject.h"
+
+class EnergyPoint : public InanimateObject
+{
+	protected :
+
+		double _radius; // radius, in pixels.
+
+		int _energyPointValue; // default: gEnergyPointValue
+		bool _energyPointValueIsLocal; // default: false -- WARNING, you have to use get/set method to actually make the energy point behave according to this flag
+		
+
+		bool _fixedLocation; // if false, location changes anytime point is harvested
+		int _internLagCounter; // when inactive, count from 0 to respawnLag
+		int _respawnLag; // time to respawn - unused for the moment.
+		bool _respawnMethodIsLocal; // default: false
+		int _respawnLagMaxValue; // default: gEnergyPointRespawnLagMaxValue (but not used as long as _respawnMethodIsLocal is false)
+
+		bool _active; // active points can be harvested, inactive points do not exist.
+		bool _agentGenerated;
+//		double _key;
+//		double _initLock;
+
+		int _iterationMax;
+
+	public :
+
+		EnergyPoint();
+		EnergyPoint(int id);
+		EnergyPoint(int id, double x, double y);
+		~EnergyPoint();
+
+		void setPosition(double x, double y);		
+		////void setPosition();
+		//Point2d getPosition();
+		bool isAgentGenerated();
+		void setAgentGenerated(bool __value);
+		bool getEnergyPointValueIsLocal();
+		void setEnergyPointValueIsLocal( bool __value );
+		int getEnergyPointValue();
+		void setEnergyPointValue( int __value );
+
+		int getRespawnLagTEST();						// not used
+		void setRespawnLag( int __lag );				// used
+
+		void setRespawnLagMaxValue( int __lagMaxValue );
+		int getRespawnLagMaxValue();		
+		void setRespawnLagMethodIsLocal( bool __value );
+		bool getRespawnLagMethodIsLocal();
+
+		void setActiveStatus( bool __value );
+		bool getActiveStatus();
+
+//		float getKey();
+//		void setKey(float key);
+		
+		void setFixedLocationStatus( bool __value ); 
+		bool getFixedLocationStatus();
+
+		void step(); // override
+
+		void display(); // override
+		void hide();	// override
+		void clean();	
+	
+};
+
+#endif
---
+++ Roborobo/prj/mEDEA-altruism/include/MedeaAltruismWorldObserver.h
@@ -0,0 +1,48 @@
+/*
+ *  WorldObserver.h
+ *  roborobo-online
+ *
+ *  Created by Nicolas on 25/05/10
+ *  moved in altruism by Jean-Marc on 17/09/2010
+ *
+ */
+
+
+
+#ifndef MEDEAALTRUISMWORLDOBSERVER_H
+#define MEDEAALTRUISMWORLDOBSERVER_H
+
+#include "RoboroboMain/common.h"
+#include "RoboroboMain/roborobo.h"
+
+#include "Observers/Observer.h"
+
+#include "mEDEA-altruism/include/MedeaAltruismSharedData.h"
+#include "mEDEA-altruism/include/MedeaAltruismAgentWorldModel.h"
+
+class World;
+
+class MedeaAltruismWorldObserver : public WorldObserver
+{
+	private:
+		void updateAllAgentsEnergyLevel();
+		double fixedCost;
+		double exponentialFactor;
+		int _agentPointCount;
+
+		double penalty();
+
+	protected:
+		int _lifeIterationCount;
+	public:
+		MedeaAltruismWorldObserver( World *__world );
+		~MedeaAltruismWorldObserver();
+				
+		void reset();
+		void step();
+		
+		int getLifeIterationCount();
+
+};
+
+#endif
